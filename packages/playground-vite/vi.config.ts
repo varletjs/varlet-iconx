c9a3f300dc99f04776b1d03f556d9c3b32686151
haoziqaq <357229046@qq.com>
2024-05-14 01:44:40 +0800

    style: cleanup

--- packages/playground-vite/vi.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from '@varlet/icon-builder'
-
-export default defineConfig({
-  name: 'i-icons',
-  namespace: 'i',
-  fontFamilyClassName: 'i--set',
-  entry: './svg-icons',
-  output: './icons',
-})
