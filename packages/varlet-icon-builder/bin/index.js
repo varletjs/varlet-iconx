1ceacb82c492cb5b26ccbbfc4b8a798293f26405
haoziqaq <357229046@qq.com>
2024-05-15 02:43:49 +0800

    fix: fix figma option

--- packages/varlet-icon-builder/bin/index.js
+++ packages/varlet-icon-builder/bin/index.js
@@ -14,6 +14,9 @@ program
   .command('figma')
   .option('-t --token <token>', 'Figma token')
   .option('-f --file <file>', 'Figma file id')
+  .option('-s --skipExisting', 'Whether to skip downloading svg files when a file with the same name exists locally')
+  .option('-c --clean', 'Whether to clear the output directory before downloading')
+  .option('-o --output', 'Output path')
   .description('Parse icons from figma')
   .action(figma)
 
