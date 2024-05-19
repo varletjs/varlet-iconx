d7e8bd34b3f3573f13b062c8693f58f1c71dc530
haoziqaq <357229046@qq.com>
2024-05-20 01:36:05 +0800

    fix: fix playground

--- packages/playground-vite/vite.config.ts
+++ packages/playground-vite/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
 import icon from '@varlet/unplugin-icon-builder/vite'
 
 export default defineConfig({
-  plugins: [icon({ onDemand: true })],
+  plugins: [vue(), icon({ onDemand: true })],
 })
